4e5945c9954d0ce2195f495fac59b5526174f9d0
赵成明 <zhaochengming@meituan.com>
2018-11-07 15:44:28 +0800

    add new project Signed-off-by: 赵成明 <zhaochengming@meituan.com>

---
+++ NLP_In_Action/Chap04_save/crf_test.bat
@@ -0,0 +1,4 @@
+@echo off
+cd C:/Users/Cigar/Documents/jupyter/NLP_learn/NLP_In_Action/Chap04_save
+crf_test -m model test.txt > test.rst
+pause
---
+++ NLP_In_Action/Chap04_save/crf_train.bat
@@ -0,0 +1,4 @@
+@echo off
+cd C:/Users/Cigar/Documents/jupyter/NLP_learn/NLP_In_Action/Chap04_save
+crf_learn -f 4 -p 8 -c 3 template train.txt model
+pause
